7ab0a389402511f08ec716603d92fe04acb6723c
AdamDagi <mugutdinovshiha02@gmail.com>
2022-02-09 13:06:29 -0500

    added navigation bar and style

--- adam-dagi-portfolio/src/components/Header.jsx
+++ adam-dagi-portfolio/src/components/Header.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Header = () => {
     return (
         <div className="header-conteiner">
-            
+            <div className="navigation-conteiner">
+                <a class="active" href="#home">HOME</a>
+                <a href="#news">PORTFOLIO</a>
+                <a href="#contact">CONTACT</a>
+                <a href="#about">RESUME</a>
+            </div>
         </div>
     );
 };
