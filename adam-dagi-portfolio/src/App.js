837c5b9f264a1fbd6d64870168bc3e77436a0fc9
AdamDagi <mugutdinovshiha02@gmail.com>
2022-02-08 23:03:02 -0500

    added main structure, copponents, images, styles and structure to main page

--- adam-dagi-portfolio/src/App.js
+++ adam-dagi-portfolio/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Header />
+      <div className="page-conteiner">
+        <div className="info-card">
+          <div className="section profile-picture-section">
+            <div className="profile-picture">
+            </div>
+          </div>
+          <div className="section profile-name-section">
+            <h1 className="profile-name">Adam Dagi</h1>
+          </div>
+          <div className="section profile-location-section">
+            <div className="profile-location-icon"></div>
+            <div className="location">New York 🇺🇸 </div>
+          </div>
+          <div className="section profile-bio-section">
+            <div className="profile-bio">
+            Ambitious student finishing Columbia Engineering Coding Boot Camp and ready for further study and progress in a new profession. 
+            Energetic, responsible, sociable with management skills in the restaurant and a Bachelor's degree in law. 
+            Flexible hard worker ready to learn and contribute to team success. 
+            Organized and motivated employee eager to apply time management and organizational skills in various environments. 
+            Seeking entry-level opportunities to expand skills while facilitating company growth.
+            My experience of working in a team will undoubtedly help me in improving the quality of the work performed, 
+            and the experience of working with clients will help me see any product from the consumer's side.     
+            </div>
+          </div>
+          <div className="section uversity-section">
+            <div className="graduation-icon"></div>
+            <div className="graduation-uni">Columbia Engineering Coding Boot Camp</div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ adam-dagi-portfolio/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+        <div className="footer-conteiner">
+        </div>
+    );
+};
+
+export default Footer;
