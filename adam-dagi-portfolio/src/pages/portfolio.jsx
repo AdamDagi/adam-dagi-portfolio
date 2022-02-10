66edb9ea1ae5e9bd1b12de2c34207f6c819e27fc
AdamDagi <mugutdinovshiha02@gmail.com>
2022-02-09 22:23:37 -0500

    working with portfolio colomns

--- adam-dagi-portfolio/src/pages/portfolio.jsx
+++ adam-dagi-portfolio/src/pages/portfolio.jsx
@@ -3,7 +3,42 @@ import React from "react";
 const Portfolio = () => {
     return (
         <>
-            dga
+            <div className="portfolio">
+                <div className="colomn-1">
+                    <div className="project-one">
+                        <ul>
+                        <a href="#" className="project-title">Project 1</a>
+                        </ul>
+                    </div>
+                    <div className="project-two">
+                        <ul>
+                        <a href="#" className="project-title">Project 2</a>
+                        </ul>
+                    </div>
+                    <div className="project-three">
+                        <ul>
+                        <a href="#" className="project-title">Project 3</a>
+                        </ul>
+                    </div>
+                </div>
+                <div className="colomn-2">
+                    <div className="project-four">
+                        <ul>
+                        <a href="#" className="project-title">Project 4</a>
+                        </ul>
+                    </div>
+                    <div className="project-five">
+                        <ul>
+                        <a href="#" className="project-title">Project 5</a>
+                        </ul>
+                    </div>
+                    <div className="project-six">
+                        <ul>
+                        <a href="#" className="project-title">Project 6</a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
