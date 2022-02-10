7927c03bbb7e655d6486fb871a5be7b78348c62e
AdamDagi <mugutdinovshiha02@gmail.com>
2022-02-09 21:08:45 -0500

    added contact page and styled that

--- adam-dagi-portfolio/src/pages/contact.jsx
+++ adam-dagi-portfolio/src/pages/contact.jsx
@@ -2,9 +2,46 @@ import React from "react";
   
 const Contact = () => {
     return (
-        <>
-            dga
-        </>
+        <div className='ContactForm'>
+            <form  className='contactForm' id='contact-form' noValidate>
+                {/* Row 1 of form */}
+                <div className='FormHeader'>
+                    <div className='NameInput'>
+                        <input
+                        type='text'
+                        name='name'
+                        className='nameInput'
+                        placeholder='Name'
+                        ></input>
+                    </div>
+                    <div className='EmailInput'>
+                        <input
+                        type='email'
+                        name='email'
+                        className='emailInput'
+                        placeholder='Email address'
+                        ></input>
+                    </div>
+                </div>
+                {/* Row 2 of form */}
+                <div className='MessageInputConteiner'>
+                    <div className='MessageInput'>
+                        <textarea
+                        rows={3}
+                        name='message'
+                        className='messageInput'
+                        placeholder='Message'
+                        ></textarea>
+                    </div>
+                </div>
+                <div className='SubmitButtonConteiner'>
+                    <button className='submit-btn' type='submit'>
+                    Submit
+                    </button>
+                </div>
+            </form>
+                        
+        </div>
     );
 };
 
